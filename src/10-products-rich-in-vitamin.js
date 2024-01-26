44a2e954ab8a6cb9b0a5e9a4dba642145a19eb47
no_name <no@email.com>
2024-01-26 10:30:28 -0300

    Requisito 10 satisfeito

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -2,6 +2,20 @@ const stockProducts = require('./data.json');
 
 const getProductsRichInVitamin = () => {
   // Desenvolva seu código dentro dessa função...
+  let products = [];
+  for (let indexProduct = 0; indexProduct < stockProducts.length; indexProduct++) {
+    const product = stockProducts[indexProduct];
+    if (typeof product.nutritionalInfo.vitamins !== 'undefined') {
+      products.push(
+        {
+          description: product.description,
+          formattedPrice: `R$ ${product.price}`,
+          vitaminsInformation: Object.entries(product.nutritionalInfo.vitamins).map((a) => `${a[0]} - ${a[1]}`)
+        }
+      );
+    }
+  }
+  return products;
 };
 
 module.exports = { getProductsRichInVitamin };
