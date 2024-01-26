3dce342fff4b0728e19eeefe44e662b98b40c7d3
b166erobot <bernard0freitas@outlook.com>
2024-01-26 09:49:31 -0300

    Requisito 9 satisfeito

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -2,6 +2,22 @@ const stockProducts = require('./data.json');
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
   // Desenvolva seu código dentro dessa função...
+  let products = [];
+  for (let indexProducts = 0; indexProducts < stockProducts.length; indexProducts++) {
+    const product = stockProducts[indexProducts];
+    if (typeof product.allergyOrIntolerance !== 'undefined') {
+      products.push(
+        {
+          description: product.description,
+          formattedPrice: `R$ ${product.price}`,
+          allergyOrIntoleranceMessage: `Pode conter: ${product.allergyOrIntolerance.join(' ')}`
+        }
+      )
+    }
+  }
+  return products;
 };
+
+console.log(getProductsWithAllergyOrIntoleranceInfo());
 
 module.exports = { getProductsWithAllergyOrIntoleranceInfo };
