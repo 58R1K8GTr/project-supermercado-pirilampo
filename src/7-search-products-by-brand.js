07d86648e0816a152b3c3985ec5f04cd0dbc9255
no_name <no@email.com>
2024-01-26 08:35:48 -0300

    Requisito 7 satisfeito

--- src/7-search-products-by-brand.js
+++ src/7-search-products-by-brand.js
@@ -1,7 +1,17 @@
 const stockProducts = require('./data.json');
 
-const searchProductsByBrand = () => {
+const searchProductsByBrand = (brand) => {
   // Desenvolva seu código dentro dessa função...
+  let filterProducts = [];
+  for (let indexProducts = 0; indexProducts < stockProducts.length; indexProducts++) {
+    const product = stockProducts[indexProducts];
+    if (product.brand === brand) {
+      filterProducts.push(
+        { description: product.description, formattedPrice: `R$ ${product.price}`}
+      );
+    }
+  }
+  return filterProducts;
 };
 
 module.exports = { searchProductsByBrand };
