31bcaeed60cb031d91c0e88ff74b2fbc1e43631a
b166erobot <bernard0freitas@outlook.com>
2024-01-25 21:57:09 -0300

    Requisito 3 satisfeito e requisito 2 faltou o ";"

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -2,6 +2,13 @@ const stockProducts = require('./data.json');
 
 const getOutOfStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
+  let productsUnavaliable = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].quantityInStock === 0) {
+      productsUnavaliable.push(stockProducts[index].productName);
+    }
+  }
+  return productsUnavaliable;
 };
 
 module.exports = { getOutOfStockProducts };
