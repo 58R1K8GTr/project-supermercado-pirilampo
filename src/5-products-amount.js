d15b7e33406c940e716aa803a5abb46bdf7357d0
no_name <no@email.com>
2024-01-25 22:19:05 -0300

    Requisito 5 satisfeito

--- src/5-products-amount.js
+++ src/5-products-amount.js
@@ -2,6 +2,12 @@ const stockProducts = require('./data.json');
 
 const getProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
+  let quantity = 0;
+  for (let indexProduct = 0; indexProduct < stockProducts.length; indexProduct++) {
+    const product = stockProducts[indexProduct];
+    quantity += product.quantityInStock;
+  }
+  return quantity;
 };
 
 module.exports = { getProductsAmount };
