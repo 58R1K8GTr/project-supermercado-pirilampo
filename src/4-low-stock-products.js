584854e1bd58cc780a47ea32a46ab3cbac14742d
b166erobot <bernard0freitas@outlook.com>
2024-01-25 22:11:58 -0300

    Requisito 4 satisfeito

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -2,6 +2,14 @@ const stockProducts = require('./data.json');
 
 const getLowStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
+  let lowStockProductsNames = [];
+  for (let indexProduct = 0; indexProduct < stockProducts.length; indexProduct++) {
+    const element = stockProducts[indexProduct];
+    if (element.quantityInStock > 0 && element.quantityInStock <= 10) {
+      lowStockProductsNames.push(`${element.productName}: ${element.quantityInStock} unidades`);
+    }
+  }
+  return lowStockProductsNames;
 };
 
 module.exports = { getLowStockProducts };
