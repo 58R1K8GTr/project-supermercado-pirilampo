4e4597e7c675ec01720f99fc42a7da028a2cc21f
no_name <no@email.com>
2024-01-25 21:39:03 -0300

    Requisito 2 satisfeito

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -2,6 +2,11 @@ const stockProducts = require('./data.json');
 
 const getUniqueProductsName = () => {
   // Desenvolva seu código dentro dessa função...
+  const names = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    names.push(stockProducts[index].productName)
+  }
+  return names;
 };
 
 module.exports = { getUniqueProductsName };
