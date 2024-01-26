ba822c164dba60e1fe87b4ccd84a609d8b3830be
b166erobot <bernard0freitas@outlook.com>
2024-01-25 22:57:54 -0300

    Requisito 6 satisfeito

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -1,7 +1,17 @@
 const stockProducts = require('./data.json');
 
-const searchProductByName = () => {
+const searchProductByName = (name) => {
   // Desenvolva seu código dentro dessa função...
+  for (let indexProduct = 0; indexProduct < stockProducts.length; indexProduct++) {
+    const product = stockProducts[indexProduct];
+    if (product.productName === name) {
+      return {
+        description: product.description,
+        formattedPrice: `R$ ${product.price}`
+      };
+    }
+  }
+  return null;
 };
 
 module.exports = { searchProductByName };
