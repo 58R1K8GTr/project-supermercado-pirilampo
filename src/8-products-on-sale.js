10c73dc48489e72a6b901a0e8366730d80ab5b7b
b166erobot <bernard0freitas@outlook.com>
2024-01-26 09:18:33 -0300

    Requisito 8 satisfeito

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -2,6 +2,20 @@ const stockProducts = require('./data.json');
 
 const getProductsOnSale = () => {
   // Desenvolva seu código dentro dessa função...
+  let products = [];
+  for (let indexProducts = 0; indexProducts < stockProducts.length; indexProducts++) {
+    const product = stockProducts[indexProducts];
+    if (product.onSale) {
+      products.push(
+        {
+          description: product.description,
+          formattedPrice: `R$ ${product.price}`,
+          onSale: product.onSale
+        }
+      );
+    }
+  }
+  return products;
 };
 
 module.exports = { getProductsOnSale };
