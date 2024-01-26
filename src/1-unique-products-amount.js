3bdf7fda90f120affbe85ffe2308aa8998d50a50
b166erobot <bernard0freitas@outlook.com>
2024-01-25 21:25:54 -0300

    Requisito 1 satisfeito

--- src/1-unique-products-amount.js
+++ src/1-unique-products-amount.js
@@ -2,6 +2,7 @@ const stockProducts = require('./data.json');
 
 const getUniqueProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
+  return stockProducts.length;
 };
 
 module.exports = { getUniqueProductsAmount };
